ba709deb295c5c30ed9212969d44b8df1ff225da
qatsindio <jmleflet@gmail.com>
2015-07-16 00:03:14 +0100

    Fixed rendering

--- week1/week-1-tessellating.js
+++ week1/week-1-tessellating.js
@@ -8,8 +8,7 @@ var transformedPoints = [];
 
 var NumTimesToSubdivide = 5;
 
-window.onload = function init()
-{
+window.onload = function init() {
     canvas = document.getElementById( "gl-canvas" );
 
     gl = WebGLUtils.setupWebGL( canvas );
@@ -18,9 +17,9 @@ window.onload = function init()
     //  Initialize our data for the Sierpinski Gasket
     // First, initialize the corners of our gasket with three points.
     var vertices = [
-        vec2( -0.5, -0.5 ),
+        vec2( -0.43, -0.25 ),
         vec2(  0,  0.5 ),
-        vec2(  0.5, -0.5 )
+        vec2(  0.43, -0.25 )
     ];
 
     divideTriangle(vertices[0], vertices[1], vertices[2], NumTimesToSubdivide);
@@ -56,7 +55,7 @@ function rotateTriangles(degrees) {
 		var v = points[i];
 		var x = v[0], y = v[1];
 
-		var d = Math.sqrt(x * x, y * y);
+		var d = Math.sqrt(x * x + y * y);
 		var amount = degrees * d;
 
 		var newX = x * Math.cos(amount) - y * Math.sin(amount);
@@ -74,8 +73,7 @@ function divideTriangle( a, b, c, count ) {
     // check for end of recursion
     if ( count === 0 ) {
         triangle( a, b, c );
-    }
-    else {
+    } else {
         //bisect the sides
         var ab = mix( a, b, 0.5 );
         var ac = mix( a, c, 0.5 );
@@ -97,10 +95,12 @@ function render() {
 }
 
 function changeDegrees(percentage) {
-	var degrees = Math.PI / 2 * percentage / 100;
+	// PI/2 is 90 degrees, multiplied by a certaing percentage
+	var degrees = Math.PI / 2 * ((percentage - 50) * 8) / 100;
 	rotateTriangles(degrees);
 
 	prepareData();
 
     render();
 }
+
